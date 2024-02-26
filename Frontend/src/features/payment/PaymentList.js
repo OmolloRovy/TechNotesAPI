590195a1b35e9f8d464b974e2673dfbb9647d45d
Vanistelrooy masime <masimevanistelrooy@gmail.com>
2024-02-26 12:48:56 +0300

    changes

--- Frontend/src/features/payment/PaymentList.js
+++ Frontend/src/features/payment/PaymentList.js
@@ -1,9 +1,62 @@
 import React from 'react'
-
+import { useGetPaymentsQuery } from "./paymentssApiSlice";
+import Payment from './ Payment'
 const PaymentList = () => {
-  return (
-    <h1>PaymentList</h1>
-  )
+  const {
+    data: payments,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } =useGetPaymentsQuery ();
+
+  let content;
+
+  if (isLoading) content = <p>Loading...</p>;
+
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>;
+  }
+
+  if (isSuccess) {
+    const { ids } = payments;
+
+    const tableContent = ids?.length
+      ? ids.map((paymentId) => <Payment key={paymentId} paymentId={paymentId} />
+        )
+      : null
+
+    content = (
+      <table className="table table--notes">
+        <thead className="table__thead">
+          <tr>
+          
+            <th scope="col" className="table__th user__username">
+              Name
+            </th>
+            <th scope="col" className="table__th user_username">
+            Amount paid
+            </th>
+            <th scope="col" className="table__th user__username">
+              Change
+            </th>
+            <th scope="col" className="table__th user__username">
+              other Method Used
+            </th>
+        
+            <th scope="col" className="table__th user__edit">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+  }
+
+  return content;
 }
 
 export default PaymentList
+
+
