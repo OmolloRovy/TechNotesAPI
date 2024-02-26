79195018c0d8d886a13cdf8c9eb9a414a1d59a6c
Vanistelrooy masime <masimevanistelrooy@gmail.com>
2024-02-26 16:27:53 +0300

    changes

--- Frontend/src/features/Customers/EditCustomer.js
+++ Frontend/src/features/Customers/EditCustomer.js
@@ -1,9 +1,18 @@
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectCustomerById } from './customersApiSlice'
+import EditCustomerForm from './EditCustomerForm'
 import React from 'react'
 
 const EditCustomer = () => {
-  return (
-    <div>EditCustomer</div>
-  )
+  const { id } = useParams()
+
+    const customer = useSelector(state => selectCustomerById(state, id))
+
+    const content = customer ? <EditCustomerForm user={customer} /> : <p>Loading...</p>
+
+    return content
 }
 
 export default EditCustomer
+
